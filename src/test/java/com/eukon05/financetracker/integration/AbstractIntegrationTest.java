37fed056689acef4090cdeafe3843b93707a47af
Łukasz Konieczny <lukimaluki@gmail.com>
2022-09-04 11:20:28 +0200

    - Improved test performance: instead of restarting the app context, the database is now cleaned up by executing an SQL statement before each test method

--- src/test/java/com/eukon05/financetracker/integration/AbstractIntegrationTest.java
+++ src/test/java/com/eukon05/financetracker/integration/AbstractIntegrationTest.java
@@ -6,9 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -16,9 +16,9 @@ import org.testcontainers.junit.jupiter.Container;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @Transactional
 @Import({IntegrationTestsUtils.class, IntegrationTestsConfiguration.class})
+@Sql(statements = "TRUNCATE TABLE app_user RESTART IDENTITY CASCADE;", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 abstract class AbstractIntegrationTest {
 
     @Autowired
