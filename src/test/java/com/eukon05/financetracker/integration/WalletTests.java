68be14f1c5edeb31d07dcaccb9d67a1b3e0fd1a9
Łukasz Konieczny <lukimaluki@gmail.com>
2022-11-21 23:11:41 +0100

    - Rewrote all tests: Stopped relying on TestContainers, the tests now run on an embedded H2 instance, launched in PostgreSQL compatibility mode. This significantly reduces execution time of the tests. Stopped populating the database with initial data before every test - now they use mocked repositories.
    - Switched from using .properties files to .yml files for configuration.

--- src/test/java/com/eukon05/financetracker/integration/WalletTests.java
+++ src/test/java/com/eukon05/financetracker/integration/WalletTests.java
@@ -1,20 +1,24 @@
 package com.eukon05.financetracker.integration;
 
-import com.eukon05.financetracker.transaction.TransactionType;
-import com.eukon05.financetracker.transaction.dto.CreateTransactionDTO;
+import com.eukon05.financetracker.transaction.TransactionRepository;
+import com.eukon05.financetracker.user.UserRepository;
+import com.eukon05.financetracker.wallet.Wallet;
 import com.eukon05.financetracker.wallet.dto.CreateWalletDTO;
 import com.eukon05.financetracker.wallet.dto.EditWalletDTO;
-import com.eukon05.financetracker.wallet.dto.WalletDTO;
-import com.eukon05.financetracker.wallet.usecase.WalletFacade;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
 
-import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
-import static com.eukon05.financetracker.auth.AuthFinals.TOKEN_PREFIX;
-import static org.junit.jupiter.api.Assertions.*;
+import static com.eukon05.financetracker.integration.IntegrationTestsUtils.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -22,114 +26,100 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 class WalletTests extends AbstractIntegrationTest {
 
-    @Autowired
-    private WalletFacade walletFacade;
+    @MockBean
+    private UserRepository userRepository;
 
-    private static final CreateWalletDTO dto = new CreateWalletDTO("test wallet", "PLN");
-    private static final EditWalletDTO editDto = new EditWalletDTO("new name", "EUR");
+    @MockBean
+    private TransactionRepository transactionRepository;
 
     @Test
     void should_create_a_new_wallet() throws Exception {
-        String token = utils.getTestAccessToken();
+        Mockito.when(userRepository.getUserByUsername(Mockito.any(String.class))).thenReturn(Optional.of(testUser));
+        CreateWalletDTO createDTO = new CreateWalletDTO("new wallet", "PLN");
 
-        mockMvc.perform(post("/wallets")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(dto))
-                        .header(AUTHORIZATION, TOKEN_PREFIX + token))
-                .andExpect(status().isCreated());
+        createTestWallet(createDTO).andExpect(status().isCreated());
 
-        assertFalse(getTestUserWallets().isEmpty());
+        assertTrue(testUser.getWallets().size() > 1);
+        testUser.getWallets().clear();
+        testUser.getWallets().add(testWallet);
     }
 
     @Test
     void should_validate_wallet_already_exists() throws Exception {
-        String token = utils.getTestAccessToken();
-        utils.createTestWallet();
+        Mockito.when(userRepository.getUserByUsername(Mockito.any(String.class))).thenReturn(Optional.of(testUser));
 
-        mockMvc.perform(post("/wallets")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(dto))
-                        .header(AUTHORIZATION, TOKEN_PREFIX + token))
-                .andExpect(status().isConflict());
+        createTestWallet(new CreateWalletDTO(testWallet.getName(), testWallet.getCurrency())).andExpect(status().isConflict());
     }
 
     @Test
     void should_delete_wallet() throws Exception {
-        String token = utils.getTestAccessToken();
-        utils.createTestWallet();
-
-        assertFalse(getTestUserWallets().isEmpty());
+        Mockito.when(userRepository.getUserByUsername(Mockito.any(String.class))).thenReturn(Optional.of(testUser));
 
         mockMvc.perform(delete("/wallets/1")
-                        .header(AUTHORIZATION, TOKEN_PREFIX + token))
+                        .header(AUTHORIZATION, utils.getDefaultToken()))
                 .andExpect(status().isOk());
 
-        assertTrue(getTestUserWallets().isEmpty());
+        assertTrue(testUser.getWallets().isEmpty());
+        testUser.getWallets().add(testWallet);
     }
 
     @Test
     void should_edit_wallet() throws Exception {
-        String token = utils.getTestAccessToken();
-        utils.createTestWallet();
+        Mockito.when(userRepository.getUserByUsername(Mockito.any(String.class))).thenReturn(Optional.of(testUser));
+        EditWalletDTO editDTO = new EditWalletDTO("new name", "USD");
 
-        mockMvc.perform(put("/wallets/1")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(editDto))
-                        .header(AUTHORIZATION, TOKEN_PREFIX + token))
-                .andExpect(status().isOk());
+        editTestWallet(editDTO).andExpect(status().isOk());
 
-        assertEquals(editDto.name(), getTestUserWallets().get(0).name());
-        assertEquals(editDto.currency(), getTestUserWallets().get(0).currency());
+        assertEquals(editDTO.name(), testWallet.getName());
+        assertEquals(editDTO.currency(), testWallet.getCurrency());
     }
 
     @Test
     void should_not_edit_wallet() throws Exception {
-        String token = utils.getTestAccessToken();
-        utils.createTestWallet();
+        Wallet wallet = new Wallet();
+        wallet.setUser(testUser);
+        wallet.setName("occupied");
+        testUser.getWallets().add(wallet);
 
-        mockMvc.perform(post("/wallets")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(editDto))
-                        .header(AUTHORIZATION, TOKEN_PREFIX + token))
-                .andExpect(status().isCreated());
+        Mockito.when(userRepository.getUserByUsername(Mockito.any(String.class))).thenReturn(Optional.of(testUser));
 
-        mockMvc.perform(delete("/wallets/1")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(editDto))
-                        .header(AUTHORIZATION, TOKEN_PREFIX + token))
-                .andExpect(status().isOk());
+        editTestWallet(new EditWalletDTO(wallet.getName(), null)).andExpect(status().isConflict());
+
+        testUser.getWallets().remove(wallet);
     }
 
     @Test
     void should_get_user_wallets() throws Exception {
-        String token = utils.getTestAccessToken();
-        utils.createTestWallet();
+        Mockito.when(userRepository.getUserByUsername(Mockito.any(String.class))).thenReturn(Optional.of(testUser));
 
         mockMvc.perform(get("/wallets")
-                        .header(AUTHORIZATION, TOKEN_PREFIX + token))
-                .andExpectAll(status().isOk(), jsonPath("$.[0].name").value(dto.name()));
+                        .header(AUTHORIZATION, utils.getDefaultToken()))
+                .andExpectAll(status().isOk(), jsonPath("$.[0].name").value(testWallet.getName()));
     }
 
     @Test
     void should_get_wallet_transactions() throws Exception {
-        String token = utils.getTestAccessToken();
-        utils.createTestWallet();
-
-        mockMvc.perform(post("/transactions")
-                        .header(AUTHORIZATION, TOKEN_PREFIX + token)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(new CreateTransactionDTO(1L, "transaction", new BigDecimal("11.5"), TransactionType.EXPENSE))))
-                .andExpect(status().isCreated());
-
-        utils.flushDatabase();
+        Mockito.when(userRepository.getUserByUsername(Mockito.any(String.class))).thenReturn(Optional.of(testUser));
+        Mockito.when(transactionRepository.getTransactionsByWallet(Mockito.any(Wallet.class), Mockito.any(Pageable.class)))
+                .thenReturn(new PageImpl<>(List.of(testTransaction)));
 
         mockMvc.perform(get("/wallets/1/transactions")
-                        .header(AUTHORIZATION, TOKEN_PREFIX + token))
-                .andExpectAll(status().isOk(), jsonPath("$.content.[0].id").value(1));
+                        .header(AUTHORIZATION, utils.getDefaultToken()))
+                .andExpectAll(status().isOk(), jsonPath("$.content.[0].name").value(testTransaction.getName()));
     }
 
-    private List<WalletDTO> getTestUserWallets() {
-        return walletFacade.getUserWalletDTOs(utils.getRegisterDTO().username());
+    private ResultActions createTestWallet(CreateWalletDTO dto) throws Exception {
+        return mockMvc.perform(post("/wallets")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto))
+                .header(AUTHORIZATION, utils.getDefaultToken()));
+    }
+
+    private ResultActions editTestWallet(EditWalletDTO dto) throws Exception {
+        return mockMvc.perform(put("/wallets/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto))
+                .header(AUTHORIZATION, utils.getDefaultToken()));
     }
 
 }
