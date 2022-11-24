8c713077bf4f33673858855c783d34d241cc61c7
Łukasz Konieczny <lukimaluki@gmail.com>
2022-11-24 19:25:06 +0100

    Refactored email use cases (reduced them to one)

--- src/main/java/com/eukon05/financetracker/email/usecase/SendRegistrationEmailUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.eukon05.financetracker.email.usecase;
-
-interface SendRegistrationEmailUseCase {
-
-    void sendRegistrationEmail(String username, String email, String rootUrl, String tokenId);
-
-}
